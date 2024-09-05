d7308278f0ff1b856131f43503c849323f40ed1d
UkashaZaheer04 <ukashazaheer.official@gmail.com>
2024-09-05 22:30:10 +0500

    dataTypes and variables

---
+++ JS/Ch-1/app.js
@@ -0,0 +1,55 @@
+let a=5;
+console.log(a);
+name="ukasha";
+console.log(name);
+ a+=5;
+ console.log(a);
+ a++;
+ console.log(a);
+  
+  let newa=++a;
+ console.log(a);
+ console.log(newa);
+ let sa=a++;
+ console.log(sa);
+ console.log(a);
+let b=2;
+b++;
+console.log(b);
+ let isadult=true;
+ if (age=18)
+ {
+    isadult=true;
+ }
+ else {
+    isadult=false;
+ }
+
+ let n="ukasha";
+ let u='ukasha';
+ console.log(n);
+ console.log(u);
+ console.log(u.length)
+ console.log  
+ let nam="TONY STARK"
+ console.log(nam[1])
+ console.log(nam.length)
+ console.log(nam[nam.length-1])
+ let firstname="ukasha"
+ let lastname="zaheer"
+ console.log(firstname+" "+lastname)
+ console.log(firstname+" "+lastname)
+ console.log(firstname[0])
+ console.log(lastname[lastname.length-1])
+ console.log(firstname+123)
+ let empty = " "
+ console.log(empty.length)
+ 
+
+
+ console.log(firstname+a)
+ let y=null
+ console.log(y)
+alert("I love you");
+console.log(alert);
+ 
