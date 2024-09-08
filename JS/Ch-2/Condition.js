320a8aab4e33081664b7da682fb219e20e1b374a
UkashaZaheer04 <ukashazaheer.official@gmail.com>
2024-09-08 13:10:02 +0500

    ifStatement

---
+++ JS/Ch-2/Condition.js
@@ -0,0 +1,31 @@
+let a=4;
+if(a<5){
+console.log("num is write")
+}
+let name="ukasha";
+if(name!="ukasha"){
+    console.log("You are not a member");
+}
+else{
+    console.log(` Welcome ${name} `);
+}
+name="ayesha";
+if(name!="ukasha"){
+    console.log("You are not a member");
+}
+else{
+    console.log(` Welcome ${name} `);
+}
+let color="yellow";
+ 
+if (color=="red"){
+    console.log(" STOP");
+}
+if( color=="yellow")
+{
+    console.log("Start");
+}
+if(color=="green")
+{
+    console.log("GO");
+}
