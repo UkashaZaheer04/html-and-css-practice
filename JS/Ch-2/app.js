3dac12bbc1d6e5f2073fdd6768dddb35a7f5e6d1
UkashaZaheer04 <ukashazaheer.official@gmail.com>
2024-09-08 11:39:47 +0500

    basics of Js

---
+++ JS/Ch-2/app.js
@@ -0,0 +1,13 @@
+let pencil=5;
+let paper=10;
+let output=`The total price is : ${pencil+paper} Rs. `;
+console.log(output);
+let age=18;
+console.log(18<0);
+console.log('123'===123);
+console.log(0 == false);
+console.log(0===false);
+console.log(123=='123');
+// 'a'=61 'B'=62-----
+// 'A'= 41 'B'=42---
+console.log('a'>'A')
