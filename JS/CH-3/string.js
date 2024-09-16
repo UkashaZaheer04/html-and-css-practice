be57a1ed719e9b01980cd6ed69dc45e9ad1cd19f
UkashaZaheer04 <ukashazaheer.official@gmail.com>
2024-09-16 09:06:09 +0500

    string methods

--- JS/CH-3/string.js
+++ JS/CH-3/string.js
@@ -1,1 +1,26 @@
- 
+//  //trim to remove extra spaces 
+//  let username=prompt("Enter username");
+
+// console.log(username.trim());
+let str="          ukasha      ";
+console.log(str.trim());
+console.log(str);
+console.log(str.toUpperCase());
+console.log(str.trim().toLowerCase());
+//index
+let msg="nhadocidjad"
+console.log(msg.indexOf("c"));
+console.log(msg.indexOf("u"));
+// to access letters of string 
+let a="ukasha";
+ console.log(a.slice(1));
+  console.log(a.slice(-3));
+ let b=a.slice(-3);
+ console.log(b);
+ console.log(msg.repeat(2))
+ let col="apnacollege";
+ console.log(col.slice(4,9));
+ console.log(col.replace('apna','our'));
+// to separete strings
+console.log(col.slice(4,11).replace('ll','tt'))
+
